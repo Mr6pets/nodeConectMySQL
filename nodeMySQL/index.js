a3affb68ad880a7271ebf098054676fe461f232b
liuan <1761330769@qq.com>
2020-03-07 13:09:05 +0800

    ..

--- nodeMySQL/index.js
+++ nodeMySQL/index.js
@@ -7,7 +7,7 @@ const app = express();
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '123456',
+  password: '',
   database: 'nodemysql'
 })
 
@@ -23,12 +23,88 @@ db.connect(function (err) {
 app.get("/createdb", (req, res) => {
   let sql = 'CREATE DATABASE nodemysql';//'CREATE DATABASE'为sql语句，'nodemysql'为数据库名称
   db.query(sql, (err, result) => {
-    if (err) {
-      console.error("err:", err);
-    }
+    if (err) throw err;
+    console.log(result);
+    res.send('server is created');
   })
 })
 
+//创建表
+app.get("/createpoststable", (res, req) => {
+  //创建一个表为posts 
+  //其中包含 id 类型是整形 自增; title body primary:主键为id
+  let sql = "CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255),body VARCHAR(255),PRIMARY KEY(id))";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("posts表已近建立");
+  })
+})
+
+//插入内容 post1
+app.get('/addpost1', (res, req) => {
+  let post = { title: 'post one', body: 'this is post one' };
+  let sql = 'INSERT INTO posts SET ?';//这里的问号可以将上面的post的值通过db.query(sql,post)添加
+  db.query(sql, post, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('内容已经插入到表中');
+  })
+})
+
+//插入内容 post2
+app.get('/addpost2', (res, req) => {
+  let post = { title: 'post two', body: 'this is post two' };
+  let sql = 'INSERT INTO posts SET ?';//这里的问号可以将上面的post的值通过db.query(sql,post)添加
+  db.query(sql, post, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('内容已经插入到表中');
+  })
+})
+
+//查询多条内容
+app.get('/getposts', (req, res) => {
+  let sql = 'SELECT * FROM posts';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("查询成功");
+    res.json(result);
+  })
+})
+
+//查询单条内容
+app.get('/getposts/:id', (req, res) => {
+  let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.json(result);
+  })
+})
+
+//更新内容
+app.get('/updatepost/:id', (req, res) => {
+  let newTitle = "update title";
+  // ${newTitle} 的内容是字符串 所以要用单引号引起来；
+  let sql = `UPDATE posts SET title='${newTitle}' WHERE id = ${req.params.id}`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.json(result);
+  })
+})
+
+//删除内容
+app.get('/deletepost/:id', (req, res) => {
+  let sql = `DELETE FROM posts WHERE id =${req.params.id}`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.json(result);
+  })
+})
 
 
 app.listen('3000', () => {
